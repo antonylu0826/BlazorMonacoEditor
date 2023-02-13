34ad47e77f91dd779b49cbfd50e7d59a5f2f7fdc
anthony <anthony@MIS-ANTHONY>
2023-02-13 08:40:40 +0800

    v0.1

---
+++ BlazorMonaco/BlazorMonaco/wwwroot/lib/vs/interop.js
@@ -0,0 +1,7 @@
+﻿function seteupmonaco() {
+    require.config({ paths: { 'vs': '/lib/vs' } });
+    monaco.editor.create(document.getElementById('container'), {
+        value: 'using System;\n\nConsole.write("Hello World");',
+        language: 'csharp'
+    });
+}
